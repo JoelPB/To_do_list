6803ac360dc4af9cf21b5e9e224e1b7b40ed9783
JoelPB <coffee.program.joel@gmail.com>
2021-08-31 11:23:48 -0300

    Até a aula Time Picker

--- app/src/main/java/joelpb/com/github/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/joelpb/com/github/todolist/ui/AddTaskActivity.kt
@@ -1,11 +1,16 @@
 package joelpb.com.github.todolist.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import joelpb.com.github.todolist.databinding.ActivityAddTaskBinding
+import joelpb.com.github.todolist.datasource.TaskDataSource
 import joelpb.com.github.todolist.extensions.format
 import joelpb.com.github.todolist.extensions.text
+import joelpb.com.github.todolist.model.Task
 import java.util.*
 
 class AddTaskActivity : AppCompatActivity() {
@@ -29,6 +34,33 @@ class AddTaskActivity : AppCompatActivity() {
                 binding.tilDate.text = Date(it + offset).format()
             }
             dataPicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+
+        binding.tilHour.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+            timePicker.addOnPositiveButtonClickListener {
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                binding.tilHour.text = "$hour:$minute"
+            }
+
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+
+        binding.btnNewTask.setOnClickListener {
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilDate.text,
+                hour = binding.tilHour.text
+            )
+            TaskDataSource.insertTask(task)
+            Log.e("TAG", "insertListeners: " + TaskDataSource.getList())
         }
     }
 }
